eb1d51ee9b5e07c1ce54f8055ed8ec7d5e0d1608
Muhamad Yazid Imani <imaniyazid@gmail.com>
2023-07-31 20:32:36 +0700

    feat: Create FilmModel class for representing film data

---
+++ app/src/main/java/com/example/moviehood/ModelFilm.java
@@ -0,0 +1,85 @@
+package com.example.moviehood;
+
+public class ModelFilm {
+    private String id;
+    private String judul;
+    private int rating;
+    private String durasi;
+    private int tahun;
+    private String genre;
+    private String negara;
+    private String deskripsi;
+    private String gambar;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDurasi() {
+        return durasi;
+    }
+
+    public void setDurasi(String durasi) {
+        this.durasi = durasi;
+    }
+
+    public int getTahun() {
+        return tahun;
+    }
+
+    public void setTahun(int tahun) {
+        this.tahun = tahun;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getNegara() {
+        return negara;
+    }
+
+    public void setNegara(String negara) {
+        this.negara = negara;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public String getGambar() {
+        return gambar;
+    }
+
+    public void setGambar(String gambar) {
+        this.gambar = gambar;
+    }
+}
